8a3c2f1865c8c9d6aae0abea082b368192396e93
donovanvidal <74463312+donovanvidal@users.noreply.github.com>
2021-12-26 18:09:06 +0100

    Update proxmox.sh

--- proxmox.sh
+++ proxmox.sh
@@ -2,11 +2,9 @@
 
 # Add a sudo user with ssh key
 adduser youser -gecos "" --disabled-password
-echo "youser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 usermod -aG sudo youser
 mkdir /home/youser/.ssh
 echo "VOTRE_CLE_SSH_PUBLIQUE" > /home/youser/.ssh/authorized_keys
-
 
 #change ssh port and rootLogin
 sed -i 's/Port\ 22/Port\ 2212/g' /etc/ssh/sshd_config
@@ -17,6 +15,8 @@ service ssh restart
 apt update
 apt upgrade -y
 apt install -y ifupdown2 openvswitch-switch fail2ban
+systemctl enable fail2ban --now
+systemctl restart networking
 
 #PVE user management
 pveum groupadd admin -comment "System Administrators"
