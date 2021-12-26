150a9ac13b12f71764d4f0f390f9329ab365d612
donovanvidal <74463312+donovanvidal@users.noreply.github.com>
2021-12-26 19:11:34 +0100

    Update proxmox.sh

--- proxmox.sh
+++ proxmox.sh
@@ -7,9 +7,9 @@ mkdir /home/youser/.ssh
 echo "VOTRE_CLE_SSH_PUBLIQUE" > /home/youser/.ssh/authorized_keys
 
 #change ssh port and rootLogin
-sed -i 's/Port\ 22/Port\ 2212/g' /etc/ssh/sshd_config
+sed -i 's/#Port\ 22/Port\ 2212/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ without-password/g' /etc/ssh/sshd_config
-service ssh restart
+systemctl restart sshd
 
 # Update and install essential packages
 apt update
@@ -22,7 +22,7 @@ systemctl restart networking
 pveum groupadd admin -comment "System Administrators"
 pveum aclmod / -group admin -role Administrator
 pveum useradd youser@pam -comment "Admin user"
-pveum usermod youser@pam -group admin
+pveum user modify youser@pam -group admin
 
 
 
